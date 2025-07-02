46166833e33298d01321db3518a4d5c6410d6b90
Nandkishor Kumar Pandit <kishornandu008@gmail.com>
2025-07-02 21:40:13 +0530

    Add root route to redirect to /listings

--- app.js
+++ app.js
@@ -81,9 +81,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi,Iam root");
-// });
+app.get('/', (req, res) => {
+  res.redirect('/listings');
+});
+
 
 app.use((req,res,next)=>{
     res.locals.currentUser = req.user;
